1bddaaa2483ddf086bf50d20a0d28ce79f3b2b74
Filipi Bardusco <filipi@bardusco.com>
2019-01-18 12:36:30 -0500

    Add files via upload

--- SpaceInvaders/core/src/PlayerShip.java
+++ SpaceInvaders/core/src/PlayerShip.java
@@ -3,10 +3,12 @@ public class PlayerShip {
 	private int x;
 	private int y;
 	
+	
 	public static final int SPEED = 5;
 	public static final int RIGHT = 1;
 	public static final int LEFT = -1;
 	public static final int SHIPLEN = 20;
+	public static final int SHIPHEIGHT = 20;
 	
 	public PlayerShip(int xPos, int yPos) {
 		//Constructor class for the ship to determine it's initial position
@@ -24,4 +26,9 @@ public class PlayerShip {
 		Bullet shot = new Bullet(Bullet.UP, x + SHIPLEN / 2, y);
 		return shot;
 	}
+	
+	/*public Rect hurtbox(){
+	 	Rect hurtbox = new Rect(x, y, SHIPLEN, SHIPHEIGHT);
+	 	return hurtbox;
+	 */
 }
