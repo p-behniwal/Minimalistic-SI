957577e68bac0d36b5b5094be2c58bc802fd4841
Filipi Bardusco <filipi@bardusco.com>
2019-01-18 13:04:48 -0500

    Add files via upload

---
+++ SpaceInvaders/core/src/SpaceInvaders/core/src/shield.java
@@ -0,0 +1,10 @@
+//Class creating "shield" objects that protect the player from incoming fire
+public class shield {
+	private int x;
+	private int y;
+	
+	public shield(int xPos, int yPos) {
+		x = xPos;
+		y = yPos;
+	}
+}
