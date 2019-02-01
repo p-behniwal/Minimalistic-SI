51665731e726d0fdbb10351c91eb23fdd8b65880
Filipi Bardusco <filipi@bardusco.com>
2019-01-31 22:01:56 -0500

    Add files via upload

--- SpaceInvaders/core/src/com/mygdx/game/PlayerShip.java
+++ SpaceInvaders/core/src/com/mygdx/game/PlayerShip.java
@@ -1,36 +1,44 @@
 package com.mygdx.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+
 //Creates, moves, an acts out player ship actions based on input received in SpaceInvaders.java
 public class PlayerShip {
-	private int x;
-	private int y;
+	private Sprite shipSprite;
+	private Texture shipPic;
 	
 	
 	public static final int SPEED = 5;
 	public static final int RIGHT = 1;
 	public static final int LEFT = -1;
-	public static final int SHIPLEN = 20;
-	public static final int SHIPHEIGHT = 20;
 	
-	public PlayerShip(int xPos, int yPos) {
+	public PlayerShip() {
 		//Constructor class for the ship to determine it's initial position
-		x = xPos;
-		y = yPos;
+		shipPic = new Texture("badlogic.jpg");
+		shipSprite = new Sprite(shipPic);				// create a Sprite from your shipPic texture
+		shipSprite.setPosition(Gdx.graphics.getWidth()/2, 10);				// set initial position
+		
 	}
 	
-	public void move(int dir) {
+	public void move() {
 		//Moves the player left or right based on input taken from SpaceInvaders.java
-		x += SPEED * dir;
-		//Rect move code
+		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT)) {
+			shipSprite.translateX(SPEED * RIGHT);
+		} else if(Gdx.input.isKeyPressed(Input.Keys.LEFT)) {
+			shipSprite.translateX(SPEED * LEFT);
+		}
 	}
 	
-	public Bullet shoot() {
+	//public Bullet shoot() {
 		//Creates a Bullet object belonging to the player and shoots it from their location
-		Bullet shot = new Bullet(Bullet.UP, x + SHIPLEN / 2, y);
-		return shot;
-	}
+		//Bullet shot = new Bullet(Bullet.UP, x + SHIPLEN / 2, y);
+		//return shot;
+	//}
 	
-	/*public Rect hurtbox(){
-	 	Rect hurtbox = new Rect(x, y, SHIPLEN, SHIPHEIGHT);
-	 	return hurtbox;
-	 */
+	public Sprite getSprite() {
+		return shipSprite;
+	}
 }
--- SpaceInvaders/core/src/com/mygdx/game/SpaceInvaders.java
+++ SpaceInvaders/core/src/com/mygdx/game/SpaceInvaders.java
@@ -6,6 +6,8 @@ import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import java.util.*;
 
@@ -17,37 +19,56 @@ public class SpaceInvaders extends ApplicationAdapter {
 	int screenX = 1024;
 	int screenY = 768;
 	ArrayList<Bullet> bullets;
+	Texture projectile;
+	Sprite shipSprite;
+
 	
-	Music music = Gdx.audio.newMusic(Gdx.files.internal("battle.mp3"));
-	Sound playerLaser = Gdx.audio.newSound(Gdx.files.internal("laser.wav")); 
-	bitMapFont font;
+	
+	
+	
+	//Music music = Gdx.audio.newMusic(Gdx.files.internal("battle.mp3"));
+	//Sound playerLaser = Gdx.audio.newSound(Gdx.files.internal("laser.wav")); 
+	BitmapFont font;
 	
 	@Override
 	public void create () {
+		batch = new SpriteBatch();
 		Gdx.graphics.setWindowedMode(screenX, screenY);
-		player = new PlayerShip(screenX / 2, screenY - 100);
 		for(int x = 100; x < screenX - 100; x += 40) {
 			for(int y = 100; y < 300; y += 40) {
 				Alien tempAlien = new Alien(x, y, Alien.CIVILIAN);
 				swarm.add(tempAlien);
 			}
 		}
-		
+		player = new PlayerShip();
+		shipSprite = player.getSprite();
 		
 		
 	}
 
 	@Override
 	public void render () {
+		batch.begin();
 		Alien.move(screenX);
 		if(Alien.pastY(screenX - 200)) {
-			//gameOver();
+			gameOver();
 		}
-		for(int i = 0; i < swarm.size(); i++) {
+		/*for(int i = 0; i < swarm.size(); i++) {
 			if(randint(0, 8) > swarm.get(i).getSpecies()) {
 				bullets.add(swarm.get(i).shoot());
 			}
-		}
+		}*/
+		
+		Gdx.gl.glClearColor(0, 0, 0, 1);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+		shipSprite.draw(batch);
+		
+		player.move();
+
+//		else if (Gdx.input.isKeyPressed(Input.Keys.SPACE)) shoot();
+
+		batch.end();
 		
 		
 		
