efa5f6b4b6f9b2abf7101f258ce06dceea2eab55
Filipi Bardusco <filipi@bardusco.com>
2019-02-06 23:16:32 -0500

    Add files via upload

--- SpaceInvaders/core/src/mygdx/game/Bullet.java
+++ SpaceInvaders/core/src/mygdx/game/Bullet.java
@@ -1,10 +1,8 @@
 //Creates and acts out bullet actions from both the player and the enemies
 package com.mygdx.game;
-import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
-import java.util.*;
+import com.badlogic.gdx.math.Rectangle;
 
 public class Bullet {
     private int dir;
@@ -14,8 +12,8 @@ public class Bullet {
     private Texture bulletImg;
 
 
-    public static final int DOWN = 1;
-    public static final int UP = -1;
+    public static final int DOWN = -1;
+    public static final int UP = 1;
     public static final int SPEED = 5;
 
     public Bullet(int direction, float xPos, float yPos) {
@@ -30,27 +28,29 @@ public class Bullet {
 
     public void move() {
         //Moves the bullet
-        bulletSprite.translateY(SPEED);
+        bulletSprite.translateY(SPEED * dir);
 
     }
 
-    public int collide(ArrayList<Alien> aliens) {
+    public int collide(Alien alien) {
         //Checks if an alien is hit by this bullet, and if so, returns the appropriate score increase
         int scoreIncrease = 0;
-        for(Alien a : aliens) {
-            if(dir == UP) {
-
-            }
-            
+        Rectangle aHurtbox = alien.getSprite().getBoundingRectangle();
+        Rectangle bHitbox = bulletSprite.getBoundingRectangle();
+        if(bHitbox.overlaps(aHurtbox) && dir == UP) {
+        	System.out.println("duck");
+        	scoreIncrease = 100 * (alien.getSpecies() + 1);
         }
         return scoreIncrease;
     }
 
-    public boolean collide() { 
+    public boolean collide(PlayerShip player) { 
         //Checks if this bullet has collided with the player, and if so, returns true signifying that the player is dead
         boolean collided = false;
-        if(dir == DOWN) {
-     
+        Rectangle pHurtbox = player.getSprite().getBoundingRectangle();
+        Rectangle bHitbox = bulletSprite.getBoundingRectangle();
+        if(dir == DOWN && bHitbox.overlaps(pHurtbox)) {
+        	collided = true;
         }
         return collided;
     }
@@ -58,7 +58,11 @@ public class Bullet {
     public float getX() {
         return x;
     }
-
+    
+    public int getDir() {
+    	return dir;
+    }
+    
     public float getY() {
         return y;
     }
